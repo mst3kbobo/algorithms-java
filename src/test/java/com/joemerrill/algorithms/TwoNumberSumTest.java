9c1ef921a1580b6c6f3ce4f6c1ea5b9d79e4da3b
Joe Merrill <joesmerrill@icloud.com>
2021-12-09 09:30:21 -0600

    Refactor TwoNumberSum comments and variable names for clarity.

--- src/test/java/com/joemerrill/algorithms/TwoNumberSumTest.java
+++ src/test/java/com/joemerrill/algorithms/TwoNumberSumTest.java
@@ -7,8 +7,9 @@ import static org.junit.jupiter.api.Assertions.*;
 class TwoNumberSumTest {
 
     @Test
-    void TwoNumberSumTest() {
+    void twoNumberSumTest() {
         TwoNumberSum tns = new TwoNumberSum();
+
         assertArrayEquals(new int[] {11, -1}, tns.twoNumberSum(new int[]{3, 5, -4, 8, 11, 1, -1, 6}, 10));
         assertArrayEquals(new int[2], tns.twoNumberSum(new int[]{0, 10, 11, -5, 6}, 12));
         assertArrayEquals(new int[] {5, 4}, tns.twoNumberSum(new int[]{5, 4, -1, 8, 6, 11}, 9));
