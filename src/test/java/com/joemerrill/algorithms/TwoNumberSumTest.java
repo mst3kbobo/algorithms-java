8d8f53b43d2f34ce16bb61e50cfccc6c01c15232
Joe Merrill <joesmerrill@icloud.com>
2022-01-26 11:51:34 -0600

    Remove instance in tests and use Class name since methods are static.

--- src/test/java/com/joemerrill/algorithms/TwoNumberSumTest.java
+++ src/test/java/com/joemerrill/algorithms/TwoNumberSumTest.java
@@ -8,10 +8,8 @@ class TwoNumberSumTest {
 
     @Test
     void twoNumberSumTest() {
-        TwoNumberSum tns = new TwoNumberSum();
-
-        assertArrayEquals(new int[] {11, -1}, tns.twoNumberSum(new int[]{3, 5, -4, 8, 11, 1, -1, 6}, 10));
-        assertArrayEquals(new int[2], tns.twoNumberSum(new int[]{0, 10, 11, -5, 6}, 12));
-        assertArrayEquals(new int[] {5, 4}, tns.twoNumberSum(new int[]{5, 4, -1, 8, 6, 11}, 9));
+        assertArrayEquals(new int[] {11, -1}, TwoNumberSum.twoNumberSum(new int[]{3, 5, -4, 8, 11, 1, -1, 6}, 10));
+        assertArrayEquals(new int[2], TwoNumberSum.twoNumberSum(new int[]{0, 10, 11, -5, 6}, 12));
+        assertArrayEquals(new int[] {5, 4}, TwoNumberSum.twoNumberSum(new int[]{5, 4, -1, 8, 6, 11}, 9));
     }
 }
--- src/test/java/com/joemerrill/algorithms/ValidateSubsequenceTest.java
+++ src/test/java/com/joemerrill/algorithms/ValidateSubsequenceTest.java
@@ -9,15 +9,24 @@ class ValidateSubsequenceTest {
     @Test
     void validateSubsequenceTest() {
 
-        ValidateSubsequence vs = new ValidateSubsequence();
+        assertTrue(ValidateSubsequence.isValidSubsequence(
+                new int[]{5, 1, 22, 25, 6, -1, 8, 10},
+                new int[]{1, 6, -1, 10}
+        ));
 
-        assertEquals(true,
-                vs.isValidSubsequence(new int[]{5, 1, 22, 25, 6, -1, 8, 10}, new int[]{1, 6, -1, 10})
-        );
-        assertEquals(false,
-                vs.isValidSubsequence(new int[]{5, 1, 22, 25, 18, -1, 8, 10}, new int[]{1, 6, -1, 10})
-        );
-        assertEquals(true, vs.isValidSubsequence(new int[]{5, 1}, new int[]{5, 1}));
-        assertEquals(false, vs.isValidSubsequence(new int[]{5, 1}, new int[]{5, 1, 8, 16}));
+        assertFalse(ValidateSubsequence.isValidSubsequence(
+                new int[]{5, 1, 22, 25, 18, -1, 8, 10},
+                new int[]{1, 6, -1, 10}
+        ));
+
+        assertTrue(ValidateSubsequence.isValidSubsequence(
+                new int[]{5, 1},
+                new int[]{5, 1}
+        ));
+
+        assertFalse(ValidateSubsequence.isValidSubsequence(
+                new int[]{5, 1},
+                new int[]{5, 1, 8, 16}
+        ));
     }
 }
